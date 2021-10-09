21fd57cd87d87bc771130cfe63c99f7dc2e88c79
akram.sahbi@esprit.tn <akram.sahbi@esprit.tn>
2021-10-09 23:47:37 +0100

    Insertion sort algorithm

---
+++ src/main/java/algorithms/sorting/Helper.java
@@ -0,0 +1,12 @@
+package main.java.algorithms.sorting;
+
+public class Helper {
+    public static void printArray(int[] array) {
+        System.out.println("*******************************************************************************************");
+        for(int value : array) {
+            System.out.print(value);
+        }
+        System.out.println("\n*******************************************************************************************");
+
+    }
+}
--- src/main/java/algorithms/sorting/InsertionSort.java
+++ src/main/java/algorithms/sorting/InsertionSort.java
@@ -1,5 +1,27 @@
 package main.java.algorithms.sorting;
 
 public class InsertionSort {
+    public static void main(String[] args) {
+        int[] array = {5,4,3,9,1,5};
+        System.out.println("The array contains: ");
+        Helper.printArray(array);
+        System.out.println("Using insertion sort to sort the array");
+        insertionSort(array);
+    }
 
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while ( j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+            System.out.println("array at iteration " + i + " :");
+            Helper.printArray(array);
+        }
+
+    }
 }
